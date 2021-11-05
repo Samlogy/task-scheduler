b6ebc4179cf3c81b27c791e3e8d780423354cf20
Samlogy <senanisammy@gmail.com>
2021-11-05 19:09:07 +0100

    add: validation

--- components/index.js
+++ components/index.js
@@ -1,3 +1,7 @@
 export { default as Task} from "./Task.jsx";
 export { default as FilterContainer} from "./FilterContainer.jsx";
 export { default as NavBar} from "./NavBar.jsx";
+export { default as Header} from "./Header.jsx";
+
+export { default as Input} from "./Input.jsx";
+export { default as CustomButton} from "./CustomButton.jsx";
---
+++ validation/index.js
@@ -0,0 +1,42 @@
+import * as yup from 'yup';
+
+// Auth
+export const loginSchema = yup.object().shape({
+    email: yup.string().email("Enter a valid Email").required("Email required"),
+    password: yup.string().required("Password required")
+});
+
+export const registerSchema = yup.object().shape({  
+    fullName: yup.string().required('Full Name required'),
+    email: yup.string().email("Enter valid Email").required("Email required"),
+    password: yup.string().required("Password is required").min(8, 'Password length must be at least 8 characters long')
+});
+    // phone: yup.string().length(10, 'Enter a valid phone number').required('phone number required'),
+    // isChecked: yup.boolean().oneOf([true], 'Conditions must be checked to sign up')
+
+export const forgotPasswordSchema = yup.object().shape({
+    email: yup.string().email("Enter valid Email").required("Email required")
+});
+
+// /* Profile */
+export const resetPasswordSchema = yup.object().shape({
+    new_password: yup.string()
+                    .required('Password required')
+                    .min(8, 'New Password length must be at least 8 characters long'),                    
+    password: yup.string()
+                    .required('Current Password required')                  
+});
+
+// export const editProfileSchema = yup.object().shape({
+//     phone: yup.string().length(10, 'Enter a valid phone number').required('phone number required'),                
+//     email: yup.string().email("Enter valid Email").required("Email required"),
+//     fullName: yup.string().required("Full Name required"),
+//     avatar: yup.string().required('Avatar is required')
+// });
+
+/* Task */
+// export const taskSchema = yup.object().shape({
+//     phone: yup.string().length(10, 'Enter a valid phone number').required('phone number required'),                
+//     email: yup.string().email("Enter valid Email").required("Email required"),
+//     full_name: yup.string().required("Full Name required"),
+// });
