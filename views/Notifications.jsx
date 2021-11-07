339d4dc55f9719a1aa4e34d490b0df020eaef863
Samlogy <senanisammy@gmail.com>
2021-11-07 16:01:50 +0100

    add: custom switches to notification screen

--- views/Notifications.jsx
+++ views/Notifications.jsx
@@ -1,66 +1,24 @@
 import React, {useEffect, useState} from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+
+import { globalStyles } from '../styles';
+import { CustomSwitch } from '../components';
 
 const Notifications = () => {
-    const [data, setData] = useState({});
+    const [emailNotifs, setEmailNotifs] = useState(false);
+    const [pushNotifs, setPushNotifs] = useState(false);
 
     const onLoad = () => {
         console.log('load personal data from api')
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.subHeader}> Allow Notifications </Text>
-            <View style={styles.listItem}>
-                <View style={styles.item}>
-                    <Text style={styles.itemLabel}> Push Notification (switch) </Text>
-                </View>
-
-                <View style={styles.item}>
-                    <Text style={styles.itemLabel}> E-mail Notification (switch) </Text>
-                </View>
-            </View>
+        <View style={globalStyles.container}>
+            <CustomSwitch text="Email Notifications" onChange={() => setEmailNotifs(prevState => !prevState)} value={emailNotifs} />
+            <CustomSwitch text="Push Notifications" onChange={() => setPushNotifs(prevState => !prevState)} value={pushNotifs} />
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        paddingHorizontal: 20,
-        paddingTop: 30
-    },
-    subHeader: {
-        fontSize: 20,
-        fontWeight: "500",
-        marginBottom: 10,
-        textAlign: "left"
-    },
-    listItem: {
-        flexDirection: "column",
-    },
-    item: {
-        flexDirection: "row",
-    },
-    itemLabel: {
-        fontSize: 16,
-        fontWeight: "500",
-        fontStyle: "italic"
-    },
-    itemValue: {
-        fontSize: 16,
-        fontWeight: "600",
-        marginLeft: ".5rem"
-    },
-    button: {
-        color: "dodgerblue",
-        borderRadius: 20,
-        height: 40,
-        width: "90%",
-        backgroundColor: "red",
-        color: "#fff"
-    },
-});
 
 export default Notifications;
