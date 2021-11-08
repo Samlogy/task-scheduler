1d8ade2e4f257464801e12f8c176d354d8640d39
Samlogy <senanisammy@gmail.com>
2021-11-08 16:37:19 +0100

    add: custom header in Home Stack

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Header } from '../components';
 import { Home, TaskDetails } from '../views';
 
-
 const Stack = createNativeStackNavigator();
+
+const options = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
 
 const HomeStack = () => {
     return(
         <NavigationContainer>
-          <Stack.Navigator>
+          <Stack.Navigator screenOptions={options}>
             <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
             <Stack.Screen name="TaskDetails" component={TaskDetails} options={{ title: 'Task Details' }} />
           </Stack.Navigator>
