e6c626aa951903ed7c9f3715e344e4239361cfec
Samlogy <senanisammy@gmail.com>
2021-11-09 09:11:02 +0100

    add: create task detail screen

--- views/TaskDetails.jsx
+++ views/TaskDetails.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from "react";
+import { SafeAreaView, StyleSheet, Text, View, Image, Dimensions } from "react-native";
 
-const TaskDetails = () => {
+import { CustomButton } from '../components';
+import { globalStyles } from "../styles";
+
+const {width} = Dimensions.get('screen');
+const imgMargin = (width - 125) / 2;
+
+const TaskDetails = ({ navigation, route }) => {
+    const data = route.params;
+    
     return(
-        'TaskDetails'
+        <View style={globalStyles.container}>
+            <Image source={require("../assets/aa.png")} style={style.img} />
+
+            <Text> {data.title} </Text>
+            <Text> {data.description} </Text>
+            <Text> {data.priority} </Text>
+            <Text> {data.status} </Text>    
+            <Text> {data.username} </Text>
+            <Text> {data.createdAt} </Text>
+            <CustomButton text="Edit" variant="filled" onPress={() => navigation.navigate('TaskForm')} />
+            <CustomButton text="Delete" variant="outline" onPress={() => {}} />
+        </View>
     )
 }
+
+const style = StyleSheet.create({
+    img: {
+      height: 125, 
+      width: 125, 
+      borderRadius: 25,
+      marginBottom: 26,
+      marginHorizontal: imgMargin
+    }
+  })
 
 export default TaskDetails;
