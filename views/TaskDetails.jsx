42c20eac85b21c19d23ee6e613d96a236dca4fc6
Samlogy <senanisammy@gmail.com>
2021-11-11 16:06:23 +0100

    add: custom modal (task details) delete action

--- views/TaskDetails.jsx
+++ views/TaskDetails.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { SafeAreaView, StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Image, Dimensions, Modal } from "react-native";
 
 import { CustomButton } from '../components';
 import { globalStyles } from "../styles";
@@ -8,11 +8,12 @@ const {width} = Dimensions.get('screen');
 const imgMargin = (width - 125) / 2;
 
 const TaskDetails = ({ navigation, route }) => {
+  const [modalVisible, setModalVisible] = useState(false);
     const data = route.params;
     
     return(
         <View style={globalStyles.container}>
-            <Image source={require("../assets/aa.png")} style={style.img} />
+            <Image source={require("../assets/aa.png")} style={styles.img} />
 
             <Text> {data.title} </Text>
             <Text> {data.description} </Text>
@@ -20,19 +21,67 @@ const TaskDetails = ({ navigation, route }) => {
             <Text> {data.status} </Text>    
             <Text> {data.username} </Text>
             <Text> {data.createdAt} </Text>
-            <CustomButton text="Edit" variant="filled" onPress={() => navigation.navigate('TaskForm')} />
-            <CustomButton text="Delete" variant="outline" onPress={() => {}} />
+            <CustomButton text="Edit" variant="filled" onPress={() => navigation.navigate('EditTask', data)} />
+            <CustomButton text="Delete" variant="outline" onPress={() => setModalVisible(true)} />
+            <CustomModal modalVisible={modalVisible} setModalVisible={setModalVisible} text="Do you want to remove this task ?" />
         </View>
     )
 }
 
-const style = StyleSheet.create({
+const CustomModal = ({ modalVisible, setModalVisible, text }) => {
+  return(
+      <Modal animationType="fade" transparent={true} visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}> {text} </Text>
+
+            <View style={{ flexDirection: "row" }}>
+              <CustomButton text="Cancel" variant="outline" onPress={() => setModalVisible(!modalVisible)} />
+              <CustomButton text="Remove" variant="filled" onPress={() => setModalVisible(!modalVisible)} />
+            </View>
+          </View>
+        </View>
+      </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
     img: {
       height: 125, 
       width: 125, 
       borderRadius: 25,
       marginBottom: 26,
       marginHorizontal: imgMargin
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
     }
   })
 
