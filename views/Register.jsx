eee326a9f6c9099ed99b646ec8cc270e40db9934
Samlogy <senanisammy@gmail.com>
2021-11-12 19:01:05 +0100

    add: checkbox in register form

--- views/Register.jsx
+++ views/Register.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
+import Checkbox from 'expo-checkbox';
 
-import { Input, CustomButton, Text } from '../components';
+import { Input, CustomButton } from '../components';
 import { registerSchema } from "../validation";
 import { globalStyles } from "../styles";
 
 const Register = () => {
+  const [isChecked, setChecked] = React.useState(false);
+
     const { control, handleSubmit, formState: { errors } } = useForm({
       resolver: yupResolver(registerSchema) 
     });
@@ -29,22 +32,48 @@ const Register = () => {
       <View style={globalStyles.container}>
         <Controller control={control} name="fullName" defaultValue=""
           render={({ field: { onChange, onBlur, value } }) => (
-            <Input onChange={onChange} value={value} placeholder="Full Name" error={errors.fullName?.message} />
+            <Input onChange={onChange} onBlur={onBlur} value={value} placeholder="Full Name" error={errors.fullName?.message} label="Full Name" />
           )}
         />
         <Controller control={control} name="email" defaultValue=""
           render={({ field: { onChange, onBlur, value } }) => (
-            <Input onChange={onChange} value={value} placeholder="Email" error={errors.email?.message} />
+            <Input onChange={onChange} onBlur={onBlur} value={value} placeholder="Email" error={errors.email?.message} label="Email" />
           )}
         />
         <Controller control={control} name="password" defaultValue=""
           render={({ field: { onChange, onBlur, value } }) => (
-            <Input onChange={onChange} value={value} placeholder="Password" error={errors.password?.message} secureTextEntry />
+            <Input onChange={onChange} onBlur={onBlur} value={value} placeholder="Password" error={errors.password?.message} label="Password" secureTextEntry autoCorrect={false} autoCapitalize="none" />
           )}
         />
+
+        <View style={styles.checkboxContainer}>
+          <Checkbox style={styles.checkbox}
+            value={isChecked}
+            onValueChange={setChecked}
+            color={isChecked ? 'coral' : "coral"}
+          />
+          <Text style={styles.text}> Accept conditions and term of use </Text>
+        </View>
+          
+
         <CustomButton text="Register" variant="filled" onPress={handleSubmit(onRegister)} />
       </View>
     );
   };
 
+
 export default Register;
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 14,
+    color: "#a5a5a5"
+  },
+  checkbox: {
+    marginVertical: 10,
+  },
+});
